cd2ed12573f9f1ede81384909d6c4f0af68d8676
sayan95 <sharthakmallik@gmail.com>
2022-05-14 23:21:25 +0530

    App configs initiated

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const MaterialComponent = [
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialComponent
+  ],
+  exports:[
+    MaterialComponent
+  ]
+})
+export class AppMaterialModule { }
